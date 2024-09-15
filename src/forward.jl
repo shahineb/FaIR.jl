a8c78a0f1cb7161e826befe27dc970c6d819fdec
Shahine <shahine@Shahines-MBP.hsd1.ma.comcast.net>
2024-09-14 23:51:08 -0400

    stable v1 gascycle model

--- src/forward.jl
+++ src/forward.jl
@@ -5,18 +5,19 @@ include("Inputs/Inputs.jl")
 
 using .GasCycle, .ForcingModel, .EnergyBalanceModel, .Inputs
 
-species = ["CO2", "CH4", "SO2", "BC"]
+species = ["CO2", "CH4"]
 E = Emissions("src/defaults/ssp245-emissions.csv", species)
 gascycle = GasCycleModel("src/defaults/gas_parameters.csv", species)
 
-
+n_species = length(species)
 n_pool = 4
-pool_partition = zeros(Real, 4, n_pool)
-E₀ = [0., 0., 2.44004844, 2.09777075]
+pool_partition = zeros(Real, n_species, n_pool)
+# E₀ = [0., 0., 2.44004844, 2.09777075]
+E₀ = [0., 0.]
 Δt = 1.
 iirfmax = 100.
-airborneₜ = zeros(Real, 4)
-Tₜ = 0
+airborneₜ = zeros(Real, n_species)
+Tₜ = 1.
 αs = zeros(Real, size(E.values))
 C = zeros(Real, size(E.values))
 for t in 1:length(E.year)
@@ -24,6 +25,22 @@ for t in 1:length(E.year)
     C[:, t], pool_partition = EtoC(gascycle, E.values[:, t], pool_partition, αs[:, t], E₀, Δt)
 end
 
+
+using GLMakie
+fig = Figure()
+units = ['ppm', 'ppb']
+for i in 1:n_species
+    ax = Axis(fig[1, i]; xlabel = "Year", ylabel = units[i],
+              title = species[i])
+    
+    # Plot the concentration over time
+    lines!(ax, E.year, C[i, :], label=species[i])
+    
+    # Enable legend
+    axislegend(ax, position=:rb)
+end
+display(fig)
+GLMakie.closeall()
 # Cₜ, Burdenₜ = EtoC(gascycle, Eₜ, Burdenₜ₋₁, αₜ, E₀, Δt)
-C
-αₜ
+# C
+# αₜ
