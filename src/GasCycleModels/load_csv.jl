09bcfa501e86acddb4fb643f630a4cb46d2da5e4
Shahine <shahine@Shahines-MBP.hsd1.ma.comcast.net>
2024-09-16 20:51:49 -0400

    added Manifest and gititgnore sandbox

--- src/GasCycleModels/load_csv.jl
+++ src/GasCycleModels/load_csv.jl
@@ -14,3 +14,36 @@ function load_gascycle_params(path::String, species::Vector{String})
     EtoC = row.emis2conc
     return (species=species, a=a, τ=τ, r0=r0, ru=ru, rT=rT, ra=ra, C₀=C₀, EtoC=EtoC)
 end
+
+
+function load_gascycle_params(path, species)
+    # speciesdf = CSV.read(path, DataFrame)
+    # species = lowercase.(species)
+    # row = filter(row -> lowercase.(row.name) ∈ species, speciesdf)
+    # a = Matrix(row[:, [:partition_fraction0, :partition_fraction1, :partition_fraction2, :partition_fraction3]])
+    # τ = Matrix(row[:, [:unperturbed_lifetime0, :unperturbed_lifetime1, :unperturbed_lifetime2, :unperturbed_lifetime3]])
+    # χ_sensitivity_τᶜᴴ⁴ = row.ch4_lifetime_chemical_sensitivity
+    # T_sensitivity_τᶜᴴ⁴ = row.lifetime_temperature_sensitivity
+end
+
+
+path = "src/defaults/species_configs_properties.csv"
+species = ["CO2", "CH4", "N2O"]
+
+speciesdf = CSV.read(path, DataFrame)
+species = lowercase.(species)
+row = filter(row -> lowercase.(row.name) ∈ species, speciesdf)
+a = Matrix(row[:, [:partition_fraction0, :partition_fraction1, :partition_fraction2, :partition_fraction3]])
+τ = Matrix(row[:, [:unperturbed_lifetime0, :unperturbed_lifetime1, :unperturbed_lifetime2, :unperturbed_lifetime3]])
+χ_sensitivity_τᶜᴴ⁴ = row.ch4_lifetime_chemical_sensitivity
+T_sensitivity_τᶜᴴ⁴ = row.lifetime_temperature_sensitivity
+r0 = row.iirf_0
+ra = row.iirf_airborne
+ru = row.iirf_uptake
+rT = row.iirf_temperature
+g0 = row.g0
+g1 = row.g1
+C₀ = row.baseline_concentration
+
+row.baseline_concentration
+row.forcing_reference_concentration
